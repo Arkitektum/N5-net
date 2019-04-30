268345814af7c091f68b7d13ffcfebaea95841cd
Henning Jensen <henning@arkitektum.no>
2019-04-30 12:29:25 +0200

    Update default localhost port number

--- arkitektum.kommit.noark5.api/Properties/Settings.Designer.cs
+++ arkitektum.kommit.noark5.api/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace arkitektum.kommit.noark5.api.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace arkitektum.kommit.noark5.api.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:49708/")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:49709/")]
         public string baseUri {
             get {
                 return ((string)(this["baseUri"]));
