b1ce197a1abd8763da9f09174f1e138316085418
Henning Jensen <henning@arkitektum.no>
2019-04-30 12:39:02 +0200

    Refactoring api controller name

--- arkitektum.kommit.noark5.api/Controllers/DokumentbeskrivelseController.cs
+++ arkitektum.kommit.noark5.api/Controllers/DokumentbeskrivelseController.cs
@@ -9,7 +9,7 @@ using System.Web.Http.OData.Query;
 
 namespace arkitektum.kommit.noark5.api.Controllers
 {
-    public class DokumentbeskrivelseController : ApiController
+    public class DokumentbeskrivelseController : System.Web.Http.ApiController
     {
         private static ODataValidationSettings _validationSettings = new ODataValidationSettings();
 
